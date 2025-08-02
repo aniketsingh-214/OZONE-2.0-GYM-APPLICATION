e3ed99e78f671d0eb3baca3368956c4a0faffcfc
ANIKET SINGH <125822570+aniketsingh-214@users.noreply.github.com>
2025-08-02 18:23:30 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://ozone-2-0-gym-application-xsx5.vercel.app/send/mail",
         {
           name,
           email,
